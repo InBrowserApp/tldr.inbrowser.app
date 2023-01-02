4a964986bb59451ae668f700b977b8971a33e307
Seedgou <i@zczc.cz>
2023-01-02 03:52:52 +0000

    add entriesCache

--- src/data/tldr-pages/zip/getEntries.ts
+++ src/data/tldr-pages/zip/getEntries.ts
@@ -1,8 +1,13 @@
 import { getZipReader } from "./getZipReader";
 import type { Entry } from "@zip.js/zip.js";
 
+let entriesCache: Entry[] | null = null;
+
 export async function getEntries(): Promise<Entry[]> {
+  if (entriesCache) return entriesCache;
+
   const zipReader = await getZipReader();
   const entries = await zipReader.getEntries();
+  entriesCache = entries;
   return entries;
 }
