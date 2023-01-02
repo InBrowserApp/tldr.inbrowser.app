01365ad54f77cbb3271181fe36dcbc985cab73e0
Seedgou <i@zczc.cz>
2023-01-02 05:09:49 +0000

    add find this page on github

---
+++ src/components/page/FindOnGitHub.vue
@@ -0,0 +1,17 @@
+<template>
+  <n-button :href="href" target="_blank" tag="a" text>
+    <template #icon>
+      <n-icon :component="Github" />
+    </template>
+    Find this page on GitHub
+  </n-button>
+</template>
+
+<script lang="ts" setup>
+import { NButton, NIcon } from "naive-ui";
+import Github from "@vicons/fa/Github";
+
+defineProps<{
+  href: string;
+}>();
+</script>
--- src/views/PageView.vue
+++ src/views/PageView.vue
@@ -1,24 +1,38 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { getPage } from "@/data/tldr-pages/page";
+import { getPage, type Page } from "@/data/tldr-pages/page";
 import { useRoute } from "vue-router";
 import { computedAsync } from "@vueuse/core";
+import { NDivider } from "naive-ui";
 import PageMarkdown from "@/components/page/PageMarkdown.vue";
+import FindOnGitHub from "@/components/page/FindOnGitHub.vue";
 
 const route = useRoute();
 const evaluating = ref(false);
 
-const markdown = computedAsync<string | undefined | null>(
+const page = computedAsync<Page | undefined | null>(
   async () => {
     try {
       const page = await getPage(route.path);
       console.debug(page);
-      const text = await page.getText();
-      console.debug(text);
-      return text;
+      return page;
     } catch (error) {
       console.error(error);
       return null;
+    }
+  },
+  undefined,
+  evaluating
+);
+
+const markdown = computedAsync<string | undefined | null>(
+  async () => {
+    if (page.value) {
+      const text = await page.value.getText();
+      console.debug(text);
+      return text;
+    } else {
+      return page.value;
     }
   },
   undefined, // initial state
@@ -29,5 +43,7 @@ const markdown = computedAsync<string | undefined | null>(
 <template>
   <main>
     <PageMarkdown :markdown="markdown" v-if="markdown" />
+    <n-divider />
+    <FindOnGitHub :href="page.githubURL" v-if="page" />
   </main>
 </template>
