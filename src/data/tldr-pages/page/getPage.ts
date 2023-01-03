5d4d1e5d090fd33abf9828ee729d00d7927ce69c
Seedgou <i@zczc.cz>
2023-01-03 06:02:33 +0000

    use Map to find Page

--- src/data/tldr-pages/page/getPage.ts
+++ src/data/tldr-pages/page/getPage.ts
@@ -1,9 +1,9 @@
 import type { Page } from "./Page";
-import { getPages } from "./getPages";
+import { getPagesMap } from "./getPagesMap";
 
 export async function getPage(path: string): Promise<Page> {
-  const pages = await getPages();
-  const page = pages.find((page) => page.path === path);
+  const pagesMap = await getPagesMap();
+  const page = pagesMap.get(path);
   if (!page) throw new Error(`Page not found: ${path}`);
   return page;
 }
---
+++ src/data/tldr-pages/page/getPagesMap.ts
@@ -0,0 +1,16 @@
+import { getPages } from "./getPages";
+import type { Page } from "./Page";
+
+let pagesMapPromise: Promise<Map<string, Page>> | null = null;
+
+export async function getPagesMap(): Promise<Map<string, Page>> {
+  if (pagesMapPromise) return await pagesMapPromise;
+
+  pagesMapPromise = (async () => {
+    const pages = await getPages();
+    const pagesMap = new Map(pages.map((page) => [page.path, page]));
+    return pagesMap;
+  })();
+
+  return await pagesMapPromise;
+}
