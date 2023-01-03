c6706d4e565255431d7b5123d03ff9470b60a1b9
Seedgou <i@zczc.cz>
2023-01-03 11:22:14 +0000

    fix loadingbar

--- src/data/tldr-pages/composables/usePlatformPages.ts
+++ src/data/tldr-pages/composables/usePlatformPages.ts
@@ -18,6 +18,7 @@ export function usePlatformPages(
         (page) =>
           page.platform === get(platform) && page.language === get(language)
       );
+      loadingBar.finish();
       return filteredPages;
     } catch (error) {
       loadingBar.error();
