8783fdf5a1b841d64798bb85de195f7463acc859
Seedgou <i@zczc.cz>
2023-01-03 12:48:36 +0000

    add PageInfoSkeleton

---
+++ src/components/platform/PageInfosSkeleton.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div v-for="i in new Array(repeat)" :key="i">
+      <n-h2>
+        <n-text code>
+          <n-skeleton text style="width: 3em" />
+        </n-text>
+      </n-h2>
+      <n-p>
+        <n-skeleton
+          style="
+            height: calc(var(--n-line-height) * var(--n-font-size));
+            vertical-align: 0;
+            max-width: 100%;
+          "
+          text
+        />
+      </n-p>
+
+      <n-divider />
+    </div>
+    <n-space justify="center">
+      <n-skeleton style="height: 28px; width: 322.636px" />
+    </n-space>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { NSkeleton, NSpace, NDivider, NH2, NText, NP } from "naive-ui";
+
+defineProps<{
+  repeat: number;
+}>();
+</script>
--- src/views/PlatformView.vue
+++ src/views/PlatformView.vue
@@ -5,6 +5,7 @@
       <PageLanguage :language="language" v-if="language !== ''" />
     </n-h1>
     <PageInfos :pages="pages" v-if="pages" />
+    <PageInfosSkeleton :repeat="5" v-else />
   </main>
 </template>
 
@@ -15,6 +16,7 @@ import { useLanguage } from "@/router/composables/useLanguage";
 import { usePlatform } from "@/router/composables/usePlatform";
 import { usePlatformPages } from "@/data/tldr-pages/composables/usePlatformPages";
 import PageInfos from "@/components/platform/PageInfos.vue";
+import PageInfosSkeleton from "@/components/platform/PageInfosSkeleton.vue";
 
 const { language } = useLanguage();
 const { platform } = usePlatform();
