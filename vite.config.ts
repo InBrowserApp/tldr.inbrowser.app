8f1080e74a6e8431bee1cafb8e9c2a69de3e8959
Seedgou <i@zczc.cz>
2023-01-03 08:11:59 +0000

    add icon and pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,41 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: [
+        "favicon.ico",
+        "favicon.svg",
+        "apple-touch-icon.png",
+        "masked-icon.svg",
+        "tldr-pages.zip",
+      ],
+      manifest: {
+        name: "tldr InBrowser.App",
+        short_name: "tldr",
+        description:
+          "tldr InBrowser.App is a web client for tldr-pages. Fully runs in your browser, no data is sent to the server. Fast and offline capable.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
