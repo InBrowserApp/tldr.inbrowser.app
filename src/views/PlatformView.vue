ace50920fe9321a599b0fbdbeff17f7420354fe9
Seedgou <i@zczc.cz>
2023-01-03 13:08:07 +0000

    use head for platform view

--- src/views/PlatformView.vue
+++ src/views/PlatformView.vue
@@ -17,8 +17,32 @@ import { usePlatform } from "@/router/composables/usePlatform";
 import { usePlatformPages } from "@/data/tldr-pages/composables/usePlatformPages";
 import PageInfos from "@/components/platform/PageInfos.vue";
 import PageInfosSkeleton from "@/components/platform/PageInfosSkeleton.vue";
+import { useHead } from "@vueuse/head";
+import { useRoute } from "vue-router";
 
 const { language } = useLanguage();
 const { platform } = usePlatform();
 const { pages } = usePlatformPages(language, platform);
+
+const route = useRoute();
+
+useHead({
+  title: `${platform.value}${
+    language.value !== "" ? ` ${language.value}` : ""
+  } | tldr InBrowser.App`,
+  meta: [
+    {
+      name: "description",
+      content: `tldr commands list for platform ${platform.value}${
+        language.value !== "" ? ` in language ${language.value}` : ""
+      }.`,
+    },
+  ],
+  link: [
+    {
+      rel: "canonical",
+      href: `https://tldr.inbrowser.app${route.path}`,
+    },
+  ],
+});
 </script>
