5e7237cb669abefb724416c59aaff98d5b8b11a0
Seedgou <i@zczc.cz>
2023-01-08 11:43:05 +0000

    remove pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import { createHead } from "@vueuse/head";
 
 import App from "./App.vue";
@@ -8,7 +7,6 @@ import router from "./router";
 const app = createApp(App);
 const head = createHead();
 
-app.use(createPinia());
 app.use(router);
 app.use(head);
 
