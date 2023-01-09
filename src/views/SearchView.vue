442f2fe464f0659bd02e9c8ca963658eb01f44f5
Seedgou <i@zczc.cz>
2023-01-09 01:51:50 +0000

    add search view

---
+++ src/views/SearchView.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <n-h2 prefix="bar" align-text>Search</n-h2>
+    <PageSearch />
+  </main>
+</template>
+
+<script setup lang="ts">
+import { NH2 } from "naive-ui";
+import PageSearch from "@/components/search/PageSearch.vue";
+import { useHead } from "@vueuse/head";
+
+useHead({
+  title: "Search | tldr InBrowser.App",
+  meta: [
+    {
+      name: "description",
+      content: "Search for tldr-pages.",
+    },
+  ],
+});
+</script>
