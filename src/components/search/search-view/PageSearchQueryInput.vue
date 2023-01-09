5e4dba1507ee6de0b30c138f116d874a4b5cf230
Seedgou <i@zczc.cz>
2023-01-09 02:40:45 +0000

    add search view

---
+++ src/components/search/search-view/PageSearchQueryInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <n-input v-model:value="query" type="text" :placeholder="placeholder" />
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { NInput } from "naive-ui";
+import { useRandomPlaceholder } from "../composables";
+
+const { placeholder } = useRandomPlaceholder();
+
+const props = defineProps<{
+  query: string;
+}>();
+
+const emit = defineEmits<{
+  (event: "update:query", value: string): void;
+}>();
+
+const query = computed({
+  get: () => props.query,
+  set: (value) => emit("update:query", value),
+});
+</script>
+
+<style>
+.page-search-auto-complete-menu .n-base-select-option__content {
+  width: 100%;
+}
+
+.page-search-auto-complete-menu.n-base-select-menu
+  .n-base-select-option.n-base-select-option--show-checkmark {
+  padding-right: calc(var(--n-option-padding-right) + 3px);
+}
+</style>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,14 +1,17 @@
 <template>
   <main>
     <n-h2 prefix="bar" align-text>Search</n-h2>
-    <PageSearch />
+    <PageSearch v-model:query="query" />
   </main>
 </template>
 
 <script setup lang="ts">
 import { NH2 } from "naive-ui";
-import PageSearch from "@/components/search/auto-complete/PageSearch.vue";
+import PageSearch from "@/components/search/search-view/PageSearch.vue";
 import { useHead } from "@vueuse/head";
+import { useRouteQuery } from "@vueuse/router";
+
+const query = useRouteQuery("query", "");
 
 useHead({
   title: "Search | tldr InBrowser.App",
