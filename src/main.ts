b439a2c6cd06be053aa1da31171d1ab9df2d2ee1
Seedgou <i@zczc.cz>
2023-02-11 08:37:19 +0000

    add GA for inbrowser.app domain only

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createHead } from "@vueuse/head";
+import VueGtag from "vue-gtag";
 
 import App from "./App.vue";
 import router from "./router";
@@ -9,6 +10,13 @@ const head = createHead();
 
 app.use(router);
 app.use(head);
+
+// Only for inbrowser.app
+if (location.hostname.endsWith("inbrowser.app")) {
+  app.use(VueGtag, {
+    config: { id: "G-NSM970Q544" },
+  });
+}
 
 // import styles
 import "@/styles/hidden-on-print.css";
