b19cc8985a12c1f7647a23eb888f3c05ce216e64
Seedgou <i@zczc.cz>
2023-10-15 20:45:21 +0800

    add inbrowser.app in navbar

--- src/components/header/nav/HeaderNav.vue
+++ src/components/header/nav/HeaderNav.vue
@@ -1,11 +1,16 @@
 <template>
   <nav class="header-nav">
     <NavGitHub />
+    <n-divider vertical />
+    <NavInBrowserApp />
   </nav>
 </template>
 
 <script lang="ts" setup>
+import { NDivider } from "naive-ui";
+
 import NavGitHub from "./NavGitHub.vue";
+import NavInBrowserApp from "./NavInBrowserApp.vue";
 </script>
 
 <style scoped>
